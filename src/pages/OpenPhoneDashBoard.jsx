437ccbd01345adbab4e007e7093d512517857e7f
seungwoo-woo <seungwoo.woo@gmail.com>
2023-10-25 17:14:00 +0900

    graph ad

--- src/pages/OpenPhoneDashBoard.jsx
+++ src/pages/OpenPhoneDashBoard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
-import { getAuth } from 'firebase/auth';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
+
+
+const data = [{name: 'Page A', uv: 400, pv: 240, amt: 2400},
+              {name: 'Page B', uv: 300, pv: 280, amt: 2400},
+              {name: 'Page C', uv: 300, pv: 210, amt: 2400},
+              {name: 'Page D', uv: 200, pv: 140, amt: 2400},
+              {name: 'Page E', uv: 280, pv: 420, amt: 2400},
+              {name: 'Page F', uv: 190, pv: 120, amt: 2400},
+            ];
 
 function OpenPhoneDashBoard(props) {
-  const auth = getAuth();
-
-  console.log('/////', auth.currentUser);
-
 
 // ------------------------------------------------------------------------------------
 // return 시작 ------------------------------------------------------------------------
@@ -16,6 +21,18 @@ return (
   <>
     <ResponsiveAppBar />
     <div>OpenPhoneDashBoard</div>
+
+    <div style={{marginTop: 50, display: 'flex', justifyContent: 'center'}}>
+      <LineChart width={600} height={300} data={data} margin={{ top: 0, right: 20, bottom: 0, left: 0 }}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </div>
   </>
 );
 
